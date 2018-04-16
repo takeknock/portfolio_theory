b399f1c0ff78cb2bca09243f233c2e1ae59a0223
mmatthew <takeknock@gmail.com>
2018-04-16 23:47:33 +0900

    use data_loader

--- script/calculator.py
+++ script/calculator.py
@@ -1,1 +1,12 @@
 import pandas as pd
+import data_loader
+
+
+def main():
+    nikkei = data_loader.load_nikkei225()
+    print(nikkei.head())
+    daw = data_loader.load_daw()
+    print(daw.head())
+
+if __name__ == '__main__':
+    main()
